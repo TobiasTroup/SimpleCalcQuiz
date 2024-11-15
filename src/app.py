d861ebc1fdfcbb4e0f5a0b661e0fe2637692d19a
tobiaskieser <tobias.m.kieser@gmail.com>
2024-11-15 20:45:54 +0100

    "Update"

--- src/app.py
+++ src/app.py
@@ -1,1 +1,46 @@
-# Text
+from operations import addition, division, multiply, substract
+
+print("Berechne zwei Zahlen: ")
+print("(A)ddition")
+print("(S)ubstraktion")
+print("(M)ultiplikation")
+print("(D)ivision\n")
+
+# Declaration
+is_answer_correct = False
+
+users_guess = ""
+result = 0
+
+operation = input("Operation: ")
+while not(operation == "A".lower() or operation == "M".lower() or operation == "S".lower() or operation == "D".lower()):
+    print("Die Operation wird nicht unterstützt!")
+    print("Wähle eine andere Operation aus.")
+    operation = input("Operation: ")
+
+number_one = input("Zahl 1: ")
+number_two = input("Zahl 2: ")
+
+while not is_answer_correct:
+    users_guess = input("Deine Antwort: ")
+
+    if users_guess == "e":
+        print("Ich beende das Spiel!")
+        break
+
+    if operation == "A".lower():
+        result = addition(number_one, number_two)
+    elif operation == "M".lower():
+        result = multiply(number_one, number_two)
+    elif operation == "S".lower():
+        result = substract(number_one, number_two)
+    elif operation == "D".lower():
+        result = division(number_one, number_two)
+
+    if result == int(users_guess):
+        print(f"Du hast Recht! Das Ergebnis ist {users_guess}.")
+        is_answer_correct = True
+    else:
+        print(f"Das Ergebnis ist nicht {users_guess}.")
+        print("Probiere es doch gleich noch einmal!")
+        continue
---
+++ src/operations.py
@@ -0,0 +1,11 @@
+def addition(number_one, number_two):
+    return int(number_one) + int(number_two)
+
+def multiply(number_one, number_two):
+    return int(number_one) * int(number_two)
+
+def substract(number_one, number_two):
+    return int(number_one) - int(number_two)
+
+def division(number_one, number_two):
+    return int(number_one) / int(number_two)
